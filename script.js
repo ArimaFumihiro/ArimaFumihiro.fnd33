391a0b07ff89d7f3ea5a5b06c765c277c07572a4
ArimaFumihiro <fumihiro_arima@mail.toyota.co.jp>
2025-01-17 18:21:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//nav
 const $nav = document.getElementsByClassName("tab-nav-item");
 const navLength = $nav.length;
 
@@ -7,9 +8,8 @@ function selectNav(e) {
   e.preventDefault();
   const $targetNav = e.target;
   const num = $targetNav.dataset.nav;
-  console.log(num);
-  
-  for (let i=0; i < navLength; i++) {
+   
+  for (let i = 0; i < navLength; i++) {
     $nav[i].classList.remove("is-active");
     const $items = document.getElementsByClassName(`item${i}`)
     for (const item of $items) {
@@ -28,6 +28,7 @@ for (let i = 0; i < navLength; i++) {
   $nav[i].addEventListener("click", selectNav);
 }
 
+//menber 呼び出し
 const inputMenber = document.getElementById("menber-file");
 const inputLink = document.getElementById("link-file")
 
@@ -40,11 +41,11 @@ function newFile(e) {
 
 function todayLeadar() {
   const leadarContent = document.getElementById("today-leadar")
-  const menbarlist = document.getElementsByClassName("menbar");
-  const menbarLength = menbarlist.length;
-  const random = Math.floor(Math.random() * menbarLength);
+  const menberlist = document.getElementsByClassName("menber");
+  const menberLength = menberlist.length;
+  const random = Math.floor(Math.random() * menberLength);
 
-  leadarContent.innerText = menbarlist[random].innerText; 
+  leadarContent.innerText = menberlist[random].innerText; 
 }
 
 function addMenber(e) {
@@ -58,13 +59,12 @@ function addMenber(e) {
     const title = document.getElementById("main-title");
     const leadar = document.getElementById("today-leadar");
     
-
     title.innerText = `${readingFile[0]}　4Sボード`;
     table.innerHTML = "<thead><tr><th>名前</th><th>カード</th><th>実施</th><th>気づき</th></tr></thead>";
     
     for (let i = 1; i < readingFileLength; i++) {
       if (typeof readingFile[i] === "string") {
-        table.innerHTML += `<tbody><tr><td class="menbar" id="menber${i}">${readingFile[i]}</td><td id="card${i}">
+        table.innerHTML += `<tbody><tr><td class="menber" id="menber${i}">${readingFile[i]}</td><td id="card${i}">
         </td><td id="situation${i}"><input type="checkbox" name="situation-checkbox" id="situation-checkbox${i}"></td><td class="flag" id="flag${i}"><input type="checkbox" name="flag-checkbox" id="flag-checkbox${i}"></td></tr></tbody>`;                
       }
     }
@@ -72,6 +72,9 @@ function addMenber(e) {
   });
 }
 
+inputMenber.addEventListener("change", addMenber);
+
+//link挿入
 function addLink(e) {
   const file = newFile(e);
   let readingFile = "";
@@ -84,20 +87,29 @@ function addLink(e) {
       const inputLink = document.getElementById(`link${i}`);
       inputLink.src = readingFile[i];
       const inputQRLink = document.getElementById(`QR${i}`);
-      inputQRLink.src = readingFile[i];
+      inputQRLink.href = readingFile[i];
     }
   });
 }
 
-inputMenber.addEventListener("change", addMenber);
 inputLink.addEventListener("change", addLink);
 
-function raiseAFlag () {
-  const flagButton = document.getElementsByClassName("flag-button");
-  const flagButtonLength = flagButton.length;
-  for (let i = 1; i <= flagButtonLength; i++) {
-    const flagButtonId = document.getElementById(`flag-button${i}`);
-    console.log(flagButtonId.value);
+
+//map配置表示
+const mapButton = document.getElementById("map-button");
+const square = document.getElementsByClassName("square");
+
+mapButton.addEventListener("click", (e) => {
+  if (mapButton.checked === false) {
+    for (let i = 0; i < square.length; i++) {
+      square[i].style.backgroundColor = "hsla(235, 100%, 3%, 0)";
+      square[i].style.color = "hsla(235, 100%, 3%, 0)";
+    }    
+  } else {
+    for (let i = 0; i < square.length; i++) {
+      square[i].style.backgroundColor = "#56565944";
+      square[i].style.color = "rgba(254, 1, 22, 0.419)";
+    }
   }
-}
+});
 
